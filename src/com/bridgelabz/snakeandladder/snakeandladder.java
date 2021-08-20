d8c34095e760be1deb811013f5faf6ca222e0e02
shruthishruu05 <shruthi.m@perfios.com>
2021-08-21 01:17:05 +0530

    ADDED:uc2 added

--- src/com/bridgelabz/snakeandladder/snakeandladder.java
+++ src/com/bridgelabz/snakeandladder/snakeandladder.java
@@ -4,6 +4,9 @@ public class snakeandladder {
 
 	public static void main(String args[]) {
 		System.out.println("Welcome to Snake and Ladder");
-		System.out.println("The Player Starts at the Position 0");
+		System.out.println("The Player Starts at Position 0");
+		int numberOnDie=0,currentPosition=0;
+		numberOnDie=(int)((Math.random()*100)%6)+1;
+		System.out.println(" Number on the Die is "+numberOnDie);
 	}
 }
