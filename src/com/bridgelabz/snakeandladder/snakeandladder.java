3ac63ae7f56dacd6fc7a7fc520bc0a69457b8f02
shruthishruu05 <shruthi.m@perfios.com>
2021-08-21 01:09:23 +0530

    ADDED: uc1 added

---
+++ src/com/bridgelabz/snakeandladder/snakeandladder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeandladder;
+
+public class snakeandladder {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome to Snake and Ladder");
+		System.out.println("The Player Starts at the Position 0");
+	}
+}
