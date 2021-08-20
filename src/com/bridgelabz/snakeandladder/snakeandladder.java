5feaf494be3c08f6bbf9228e8b809949746d4a67
shruthishruu05 <shruthi.m@perfios.com>
2021-08-21 01:24:58 +0530

    ADDED: uc5 added

--- src/com/bridgelabz/snakeandladder/snakeandladder.java
+++ src/com/bridgelabz/snakeandladder/snakeandladder.java
@@ -6,15 +6,15 @@ public class snakeandladder {
 	public static final int LADDER=1;
 	public static final int SNAKE=2;
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome ");
 		System.out.println("The Player Starts at Position 0");
 		int numberOnDie=0,currentPosition=0;
 		while(currentPosition<100)
 		{
 			numberOnDie=(int)((Math.random()*100)%6)+1;
-			System.out.println("The Number on the Die is: "+numberOnDie);
-			int choice=(int)(Math.random()*100)%3;
-			switch(choice) {
+			System.out.println("The Number on the Die is "+numberOnDie);
+			int option=(int)(Math.random()*100)%3;
+			switch(option) {
 				case NOPLAY:
 					System.out.println("The Option is No Play");
 					break;
@@ -31,9 +31,12 @@ public class snakeandladder {
 			if(currentPosition<0) {
 				currentPosition=0;
 			}	
+			if(currentPosition>100) {
+				currentPosition=currentPosition-numberOnDie;
+			}
 			System.out.println("The Current Position "+currentPosition);
 		}
-		System.out.println("The Player reached Winning position");
+		System.out.println("The Player reached the Winning position");
 		
 		
 
@@ -41,3 +44,4 @@ public class snakeandladder {
 		
 		
 }
+
